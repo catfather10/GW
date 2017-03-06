43c429c0089164f8e382f4561324c56a45b64d90
Maciej Ossowski <catfather10@gmail.com>
2017-03-06 16:49:41 +0100

    dodano 3d

---
+++ 3d.py
@@ -0,0 +1,35 @@
+#from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import numpy as np
+def plotHist2D():
+    sampleData=np.loadtxt("data/v3/SNRv3_M1NNA800_sample10000.gz")
+    maxSNR=max(sampleData[:,0])
+    minSNR=8
+    maxMz=max(sampleData[:,4])
+    minMz=1
+    binsSNR=200
+    binsMz=200
+#    xedges = np.linspace(minSNR,maxSNR,binsSNR)
+#    yedges = np.linspace(minMz,maxMz,binsMz)
+    xedges=np.logspace(start=np.log10(minSNR),stop=np.log10(maxSNR),num=binsSNR)
+    yedges=np.logspace(start=np.log10(minMz),stop=np.log10(maxMz),num=binsMz)
+    x=sampleData[:,0]
+    y=sampleData[:,4]
+    fig = plt.figure(figsize=(7, 3))
+    H, xedges, yedges = np.histogram2d(x, y, bins=(xedges, yedges))
+    H = H.T  # Let each row list bins with common y range.
+
+    ax = fig.add_subplot(111, title="SNR-Mz_LogLogLog")
+    plt.xlabel('SNR')
+    plt.ylabel('Mz')
+
+    X, Y = np.meshgrid(xedges, yedges)
+    p=ax.pcolormesh(X, Y, H,norm=colors.LogNorm(vmin=1, vmax=H.max()))
+    plt.gca().set_xscale("log")
+    plt.gca().set_yscale("log")
+    cbar=plt.colorbar(p)
+    cbar.ax.set_ylabel('dN/dMz dSNR')
+    plt.show()
+    
+plotHist2D()
